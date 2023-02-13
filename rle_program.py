2b094ec5c62e72c17634d5b360bebc2ed70f3fc1
Carson Johnson <carsonjohnson@Carsons-MacBook-Pro.local>
2023-02-13 14:31:22 -0500

    Print Spectrum Image

---
+++ rle_program.py
@@ -0,0 +1,28 @@
+def print_menu():
+
+    print('Welcome to the RLE image encoder!\n'
+          '\n'
+          'Displaying Spectrum Image:\n'
+          'DISPLAY IMAGE HERE\n'
+          '\n'
+          '\n'
+          'RLE Menu\n'
+          '--------\n'
+          '0. Exit\n'
+          '1. Load File\n'
+          '2. Load Test Image\n'
+          '3. Read RLE String\n'
+          '4. Read RLE Hex String\n'
+          '5. Read Data Hex String\n'
+          '6. Display Image\n'
+          '7. Display RLE String\n'
+          '8. Display Hex RLE Data\n'
+          '9. Display Hex Flat Data\n'
+          '\n')
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_menu()
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
